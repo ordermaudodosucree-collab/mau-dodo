198750dd27763235c0f31d788f1f41dbedb23875
Soreinza Malepa <marie.soreinza.malepa@gmail.com>
2026-07-07 18:17:08 +0200

    Fix doublon chargerBesoins

--- frontend/src/Kanban.js
+++ frontend/src/Kanban.js
@@ -40,14 +40,6 @@ export default function Kanban() {
     setActiveId(null);
   };
 
-const chargerBesoins = async (commandeId) => {
-  try {
-    const res = await axios.get(`${API}/commandes/${commandeId}/besoins`);
-  } catch (e) {
-    setBesoins([]);
-  }
-};
-
   const ouvrirCarte = (id) => {
   setActiveId(prev => prev === id ? null : id);
   if (activeId !== id) {
